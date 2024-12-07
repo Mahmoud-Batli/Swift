3160475c82fbced7aebb3e262ff16f2118491674
Mahmoud Batli <79056741+Mahmoud-Batli@users.noreply.github.com>
2024-12-07 20:47:33 +0200

    Create Variables & Constants.swift

---
+++ 01 Variables & Constants.swift
@@ -0,0 +1,18 @@
+//Variables
+var firstName = "Mahmoud"
+var lastName : String = "Batli"
+
+var age : Int
+age = 30
+
+var code : Float = 9.45
+var degree : Double = 5.0998976783
+
+var hasJop = false
+var isMarried : Bool
+isMarried  = true
+
+print("My name is \(firstName + " " + lastName)") ; print("I'm \(age) years old")
+
+//Constants
+let nationality = "Egyptian"
