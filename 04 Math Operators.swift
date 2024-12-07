2d2bcc799da25dc9bb0e4a4014baf71364b6b96c
Mahmoud Batli <79056741+Mahmoud-Batli@users.noreply.github.com>
2024-12-07 22:07:07 +0200

    Create Math Operators.swift

---
+++ 04 Math Operators.swift
@@ -0,0 +1,16 @@
+var a = 1
+var b = 2.5
+print(a + Int(b))
+
+var x = 10 , y = 20 , z = 30
+print("x + y = \(x+y)")
+print("x - y = \(x-y)")
+print("x * y = \(x*y)")
+print("x / y = \(x/y)")
+
+a = a+1
+a += 1
+a -= 1
+a *= 1
+a /= 1
+print(a)
